4c5dce515eba7cf808d3b05bf7d6db679ff19bb4
Ben Bagley <ben@benbagley.co.uk>
2018-01-09 18:22:07 +0000

    slight restructure

--- app.js
+++ app.js
@@ -1,19 +1,29 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const nodemailer = require('nodemailer');
 const hbs = require('hbs');
 const fs = require('fs');
 
-const port = process.env.PORT || 3000;
 var app = express();
 
+// Server port
+const port = process.env.PORT || 3000;
+// Server starting message
+app.listen(port, () => {
+  console.log(`Server is up on port ${port}`);
+});
+
+// Views directory established and handbars engine
 hbs.registerPartials(__dirname + '/views/layouts')
 app.set('view engine', 'hbs');
+
+// static assets rendered
+app.use(express.static(__dirname + '/public'));
 
 // body-parser middleware
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
+// server.log setup middleware
 app.use((req, res, next) => {
   var now = new Date().toString();
   var log = `${now}: ${req.method} ${req.url}`
@@ -28,78 +38,11 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use(express.static(__dirname + '/public'));
+// Routes
+const routes = require('./routes/routes');
+app.use("/", routes);
 
+// Get year for footer
 hbs.registerHelper('getCurrentYear', () => {
   return new Date().getFullYear()
-});
-
-// Routes
-app.get('/', (req, res) => {
-  res.render('index.hbs', {
-    pageTitle: 'homepage'
-  });
-});
-
-app.get('/registration', (req, res) => {
-  res.render('registration.hbs', {
-    pageTitle: 'Registration'
-  });
-});
-
-app.get('/login', (req, res) => {
-  res.render('login.hbs', {
-    pageTitle: 'Login'
-  });
-});
-
-app.post('/send', (req, res) => {
-  const output = `
-    <p>You have a new contact request</p>
-    <h3>Contact Details</h3>
-    <br>
-    <ul>
-    <li>Name: ${ req.body.name }</li>
-    <li>Name: ${ req.body.email }</li>
-    </ul>
-
-    <h3>Message:</h3>
-    <p>${ req.body.message }</p>
-  `;
-
-  nodemailer.createTestAccount((err, account) => {
-    // create reusable transporter object using the default SMTP transport
-    let transporter = nodemailer.createTransport({
-      host: "smtp.mailtrap.io",
-      port: 2525,
-      auth: {
-        user: "a3ad3f28b0982c",
-        pass: "64f3d499b1d6d2"
-      }
-    });
-    // setup email data with unicode symbols
-    let mailOptions = {
-      from: `"${req.body.name}" <${req.body.email}>`, // sender address
-      to: 'poetryoutloudpr.icp@gmail.com', // list of receivers
-      subject: 'New contact message', // Subject line
-      html: output // html body
-    };
-    // send mail with defined transport object
-    transporter.sendMail(mailOptions, (error, info) => {
-      if (error) {
-        return console.log(error);
-      }
-
-      console.log('Message sent: %s', info.messageId);
-      console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
-
-      res.render('index', {
-        pageTitle: 'Thank you'
-      });
-    });
-  });
-});
-
-app.listen(port, () => {
-  console.log(`Server is up on port ${port}`);
 });
---
+++ routes/routes.js
@@ -0,0 +1,69 @@
+const express = require('express');
+const nodemailer = require('nodemailer');
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  res.render('index.hbs', {
+    pageTitle: 'homepage'
+  });
+});
+
+router.get('/registration', (req, res) => {
+  res.render('registration.hbs', {
+    pageTitle: 'Registration'
+  });
+});
+
+router.get('/login', (req, res) => {
+  res.render('login.hbs', {
+    pageTitle: 'Login'
+  });
+});
+
+router.post('/send', (req, res) => {
+  const output = `
+    <p>You have a new contact request</p>
+    <h3>Contact Details</h3>
+    <ul>
+    <li>Name: ${ req.body.name }</li>
+    <li>Name: ${ req.body.email }</li>
+    </ul>
+
+    <h3>Message:</h3>
+    <p>${ req.body.message }</p>
+  `;
+
+  nodemailer.createTestAccount((err, account) => {
+    // create reusable transporter object using the default SMTP transport
+    let transporter = nodemailer.createTransport({
+      host: "smtp.mailtrap.io",
+      port: 2525,
+      auth: {
+        user: "a3ad3f28b0982c",
+        pass: "64f3d499b1d6d2"
+      }
+    });
+    // setup email data with unicode symbols
+    let mailOptions = {
+      from: `"${req.body.name}" <${req.body.email}>`, // sender address
+      to: 'poetryoutloudpr.icp@gmail.com', // list of receivers
+      subject: 'New contact message', // Subject line
+      html: output // html body
+    };
+    // send mail with defined transport object
+    transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        return console.log(error);
+      }
+
+      console.log('Message sent: %s', info.messageId);
+      console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+
+      res.render('index', {
+        pageTitle: 'Thank you'
+      });
+    });
+  });
+});
+
+module.exports = router;
