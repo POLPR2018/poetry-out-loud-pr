eb56bda12c12b804283d3fcf5b6c774642c9189c
Ben Bagley <ben@benbagley.co.uk>
2018-01-13 15:11:21 +0000

    removed console.log

--- models/user.js
+++ models/user.js
@@ -76,8 +76,6 @@ module.exports.getUserById = function(id, callback){
 }
 
 module.exports.comparePassword = function(candidatePassword, hash, callback) {
-  console.log(candidatePassword)
-  console.log(hash)
   bcrypt.compare(candidatePassword, hash, function(err, isMatch) {
     if(err) throw err;
     callback(null, isMatch);
